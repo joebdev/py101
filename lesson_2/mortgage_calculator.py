6fc0ad8605d7f26a50918defe484eec570918c11
Joe <joebbaney@gmail.com>
2025-01-27 15:01:21 -0600

    fixed some of the errors and edge cases in mortgage_calculator

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -21,39 +21,40 @@ def mortgage_calculator():
     # Ask the user for the loan price
         prompt("How much is your loan?")
         prompt("Example: 1_000_000 for 1000000")
-        loan_amount = float(input())
+        loan_amount = input()
 
         while invalid_num(loan_amount):
             prompt("Please enter a valid loan amount")
-            loan_amount = float(input())
+            loan_amount = input()
 
         # Find the APR <-- Get APR as a numerical value and
             # then divide it by 100 to get the percentage
         prompt("What is your interest rate?")
         prompt("Example: 5 for 5% or 7.5 for 7.5%")
-        apr = (float(input())) / 100
+        apr = input()
 
         while invalid_num(apr):
             prompt("Please enter a valid APR")
-            apr = float(input())
+            apr = input()
 
         # Find the length of the loan in years
         prompt("How many years is your loan?")
-        loan_duration = float(input())
+        loan_duration = input()
 
         while invalid_num(loan_duration):
             prompt("Please enter a valid number of years")
-            loan_duration = float(input())
+            loan_duration = input()
 
         # Take the duration in years and divide it by 12 to get the
             # monthly duration
         loan_duration_in_months = float(loan_duration) * 12
 
         # Divide the apr by 12 to get the monthly interest rate
-        monthly_interest_rate = apr / 12
+        annual_interest_rate = float(apr) / 100
+        monthly_interest_rate = annual_interest_rate / 12
 
         # Calculate the monthly payment using the formula given
-        monthly_payment = loan_amount * (
+        monthly_payment = float(loan_amount) * (
             monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (
                 -loan_duration_in_months)))
 
@@ -62,6 +63,7 @@ def mortgage_calculator():
 
         # Ask the user if they would like to calculate another payment
         prompt('Would you like to calculate another payment?')
+        prompt('Please enter "yes" or "no".')
 
         # if the input is yes, it'll rerun, if anything else, exit
         user_input = input()
